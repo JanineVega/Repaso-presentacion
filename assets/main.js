6eae6632ddc6b7cf6e9ad6192ff7c8505bad8830
Janine Vega <osmary.vega@gmail.com>
2017-03-30 14:29:10 -0500

    empezando

--- assets/main.js
+++ assets/main.js
@@ -2,4 +2,7 @@ window.onload=function(){
   document.getElementById("registrate").addEventListener("click",function(){
     window.location.href="registrar.html";
   });
+  document.getElementById("login").addEventListener("click",function(){
+    window.location.href="iniciarSesion.html";
+  });
 };
